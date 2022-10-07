26785ea95ea74dbe43f8ab46dd3dcf5d832475c3
mykhailolapshyn <mykhailolapshyn@MYKHAILOLAPBF28>
2022-10-07 17:33:49 +0200

    Добавьте файлы проекта.

---
+++ GeoObject/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Geo
+{
+    abstract class GeoObj
+    {
+        protected int X_coor;
+        protected int Y_coor;
+        protected string name;
+        protected string description;
+
+        public GeoObj(int X_coor, int Y_coor, string name, string description)
+        {
+            this.X_coor = X_coor;
+            this.Y_coor = Y_coor;
+            this.name = name;
+            this.description = description;
+        }
+        
+        public virtual void GetInformation()
+        {
+            Console.WriteLine($"X: {X_coor}\nY: {Y_coor}\nName: {name}\nDescription: {description}");
+        }
+    }
+    class River: GeoObj
+    {
+        private double speed;
+        private int lenght;
+
+        public River(int X_coor, int Y_coor, string name, string description, double speed, int lenght): base(X_coor, Y_coor, name, description)
+        {
+            this.speed = speed;
+            this.lenght = lenght;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.Write($"Speed: {speed}\nLength: {lenght}");
+        }
+    }
+    class Mountain : GeoObj
+    {
+        private int highest_point;
+        public Mountain(int X_coor, int Y_coor, string name, string description, int highest_point): base(X_coor, Y_coor, name, description)
+        {
+            this.highest_point = highest_point;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine($"Highest point: {highest_point}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            River river = new River(120, 214, "Dnipro", "The longest river in Ukraine", 25.5, 5000);
+            Mountain mountain = new Mountain(345, 654, "Hoverla", "The highest mountain in Ukraine", 2000);
+            Console.Write("Object river:\n");
+            river.GetInformation();
+            Console.WriteLine("\n");
+            Console.Write("Object mountain:\n");
+            mountain.GetInformation();
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,86 @@
+﻿public interface Geographical_Object
+{
+    public double X { get; set; }
+
+    public double Y { get; set; }
+    public string name { get; set; }
+    public string des { get; set; }
+
+    public void GetInformation();
+
+}
+class River : Geographical_Object
+{
+    private double x;
+    private double y;
+    private string Name;
+    private string Des;
+    private double speed;
+    private double len;
+    public double X { get => x; set => x = value; }
+    public double Y { get => y; set => y = value; }
+    public string name { get => Name; set => Name = value; }
+    public string des { get => Des; set => Des = value; }
+
+    public double Speed { get => speed; set => speed = value; }
+    public double Len { get => len; set => len = value; }
+
+    public void GetInformation()
+    {
+        Console.Write($"X: {X}\nY: {Y}\nName: {name}\nDescription: {des}\nSpeed: {speed}\nLength: {len}");
+    }
+}
+class Mountain : Geographical_Object
+{
+    private double x_coor;
+    private double y_coor;
+    private string _name;
+    private string _des;
+    private double highest_point;
+
+    public double X { get => x_coor; set => x_coor = value; }
+    public double Y { get => y_coor; set => y_coor = value; }
+
+    public string name { get => _name; set => _name = value; }
+
+    public string des { get => _des; set => _des = value; }
+
+    public double Highest { get => highest_point; set => highest_point = value; }
+
+    public void GetInformation()
+    {
+        Console.Write($"X: {X}\nY: {Y}\nName: {name}\nDescription: {des}\nHighest point: {Highest}");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        River river = new River(); // object river;
+        Mountain mountain = new Mountain(); // object mountain 
+
+        // Setting values to the river object
+        river.name = "Dnipro";
+        river.Speed = 50;
+        river.X = 110;
+        river.Y = 60;
+        river.Len = 5000;
+        river.des = "The longest river in Ukraine";
+
+        // Setting values to the mountain object 
+        mountain.name = "Hoverla";
+        mountain.Highest = 2000;
+        mountain.X = 136;
+        mountain.Y = 111;
+        mountain.des = "The highest mountain in Ukraine, which is located in Carpathian";
+
+        // Print information about object river
+        Console.Write("River:\n");
+        river.GetInformation();
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Mountain:");
+        // Print information about object mountain
+        mountain.GetInformation();
+    }
+}
